db333305e92b53067ff32d3eb3e894cfd0e78c60
shelchin <hi@shelchin.com>
2024-06-15 15:07:43 +0800

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: cloudflare()
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  // output: "server",
+  // adapter: cloudflare()
 });
